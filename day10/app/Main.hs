bee86de0430e220dde4c642b05c274bca444ed3c
Nina Scheffler <ninaisgoingplaces@icloud.com>
2025-12-12 00:48:14 +0100

    Update Main.hs
    Brute force. Took roughly 1 hour on my macbook pro.

--- day10/app/Main.hs
+++ day10/app/Main.hs
@@ -7,13 +7,17 @@
 module Main where
 
 
-import Data.List (subsequences)
+import Data.List (subsequences, elemIndex)
 import Data.Foldable (foldl')
+import Data.Maybe (fromJust)
+--import Control.Monad (guard)
+import Data.Set (fromList, toList, difference)
+--import Control.Monad (forM)
 
 
 main :: IO ()
 main = do
-    part1
+    part2
 
 split :: Eq a => a -> [a] -> [[a]]
 split _ [] = []
@@ -33,11 +37,14 @@ parts xs =
 innerString :: String -> String
 innerString s = take (length s - 2) $ drop 1 s
 
+type Lights = [Int]
 
-makeLights :: String -> [Int]
+makeLights :: String -> Lights
 makeLights = map (\c -> if c == '.' then 0 else 1) . innerString
 
-makeButtons :: String -> [Int]
+type Buttons = [Int]
+
+makeButtons :: String -> Buttons
 makeButtons s =
     let s' = innerString s
         vals = map read $ split ',' s' :: [Int]
@@ -50,19 +57,32 @@ makeButtons s =
     run (n:ns) i acc          = run (n:ns) (i+1) (0:acc)
     run []     _ acc          = acc
 
+type Joltages = [Int]
 
-parseLine :: (String, [String], String) -> ([Int], [[Int]], [Int])
-parseLine (sLights, sButtons, joltages) =
+makeJoltage :: String -> Joltages
+makeJoltage sJt = map read . split ',' $ innerString sJt
+
+
+parseLine :: (String, [String], String) -> (Lights, [Buttons], Joltages)
+parseLine (sLights, sButtons, sJoltages) =
     let lights = makeLights sLights
         buttons = map makeButtons sButtons
+        maxButton = maximum $ map length buttons
+        safety = replicate maxButton 0 :: [Int]
+        buttons' = map (++ safety) buttons
+        joltages = makeJoltage sJoltages
     in
-        (lights, buttons, [])
+        (lights, buttons', joltages)
 
-addButtons :: [Int] -> [Int] -> [Int]
+
+addButtons :: Buttons -> Buttons -> Buttons
 addButtons (x:xs) (y:ys) = (x + y):addButtons xs ys
 addButtons (x:xs) []     = x:addButtons xs []
 addButtons []     (y:ys) = y:addButtons [] ys
 addButtons []     []     = []
+
+addAllButtons :: [Buttons] -> Buttons
+addAllButtons = foldl' addButtons []
 
 removeTrailingZeros :: [Int] -> [Int]
 removeTrailingZeros = reverse . dropWhile (==0) . reverse
@@ -71,7 +91,7 @@ solveLine :: [Int] -> [[Int]] -> [[[Int]]]
 solveLine target buttons =
    let
         bs = subsequences buttons
-        res = map (removeTrailingZeros . map (`mod` 2) . foldl' addButtons []) bs
+        res = map (removeTrailingZeros . map (`mod` 2) . addAllButtons) bs
         target' = removeTrailingZeros target
         solutions = map snd $ filter ((==target') . fst) $ zip res bs
     in
@@ -92,3 +112,91 @@ part1 = do
 
 -- --------------------------
 
+minimumAt :: [Int] -> (Int, Int)
+minimumAt xs =
+    let x0 = minimum $ filter (/= 0) xs
+    in (fromJust $ elemIndex x0 xs, x0)
+
+maximumAt :: [Int] -> (Int, Int)
+maximumAt xs =
+    let x0 = maximum $ filter (/= 0) xs
+    in (fromJust $ elemIndex x0 xs, x0)
+
+part2 :: IO ()
+part2 = do
+    --rows <- lines <$> readFile "example.txt"
+    rows <- lines <$> readFile "puzzle_input.txt"
+    let rows' = map (parts . split ' ') rows
+
+ 
+
+
+    let results = do row <- take 3 rows'
+                     let (_, buttons, joltages) = parseLine row
+                     return $ getResult joltages buttons
+
+    print results
+    print $ sum results
+
+
+
+multButton :: Int -> Buttons -> Buttons
+multButton n = map (*n)
+
+getResult :: Joltages -> [Buttons] -> Int
+getResult joltages bs =
+    let
+        res = run2 joltages bs
+    in
+        if not (null res) then minimum $ map sum res else -1
+
+
+
+minimum0 :: [Int] -> Int
+minimum0 [] = 0
+minimum0 xs =
+    let xs' = filter (/=0) xs
+    in if null xs' then 0 else minimum xs'
+
+step :: Joltages -> [Buttons] -> (Int, [Joltages], [Buttons])
+step joltages bs =
+    let
+        bSum = addAllButtons bs
+        (i, _) = minimumAt bSum
+        jMax = joltages !! i
+
+        --(i, jMin) = minimumAt joltages
+        rlvButtons = filter (\b -> b !! i == 1) bs
+        presses = possis jMax rlvButtons
+
+        possibilities = map (\p -> map (uncurry multButton) $ zip p rlvButtons ) presses
+        combined = map ((\b -> map ((-1) *) b) . addAllButtons) possibilities
+        posJoltages = (toList . fromList) $ filter (all (>=0)) $ map (addButtons joltages) combined
+
+        bs' = toList $ difference (fromList bs) (fromList rlvButtons)
+    in
+        (jMax, posJoltages, bs')
+
+possis :: Int -> [a] -> [[Int]]
+possis n [_]    = [[n]]
+possis n (_:xs) = do
+        j <- [0..n]
+        js <- possis (n-j) xs
+        return $ j:js
+possis _ []     = []
+
+
+
+run2 ::  Joltages -> [Buttons] -> [[Int]]
+run2 []       _  = [[]]
+run2 joltages _  | all (==0) joltages = [[]]
+run2 joltages bs =
+    let
+        (jMin', posJoltages', bs') = step joltages bs
+        res = do {
+            joltages' <- posJoltages';
+            path <- run2 joltages' bs';
+            return $ jMin':path
+        }
+    in
+        res
